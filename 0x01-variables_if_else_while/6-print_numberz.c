394c426281e649680c5078b2e2eadb9fff0930a0
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-17 21:59:02 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers in base 10 starting from 0
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
