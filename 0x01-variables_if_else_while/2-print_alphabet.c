f158577848e281b95bd5b4489e9319f595cc9a24
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-17 21:35:53 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabets in lowercase
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
