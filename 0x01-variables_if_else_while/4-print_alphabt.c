1d8590959fa1cf8a1a334386d0eab92a7e3b0c66
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-17 21:46:35 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * main - Prints alphabets in lowercase and creates a new line
+ *
+ * Return: 0
+*/
+
+imt main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
