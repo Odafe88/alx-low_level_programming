d5fbf71faa8a951bcea2a3a60d7051265de6d2b1
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-17 20:46:10 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print a code without using printf or puts
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 61);
+	return (1);
+}
