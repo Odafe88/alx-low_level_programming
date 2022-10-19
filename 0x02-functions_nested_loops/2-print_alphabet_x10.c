6a40c26490c0ff3318ac9dba536856ef824ef307
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-19 02:55:10 -0700

    update task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - Prints ten times the alphabet
  *
- * Return: Always 0
+ *
 */
 
 void print_alphabet_x10(void)
@@ -14,12 +14,10 @@ void print_alphabet_x10(void)
 	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
-		{
 			_putchar(la);
-		}
 
 		_putchar('\n');
 	}
 
-	return (0);
+
 }
