864921c1d43bf3ff295970a7b624b395f3ee0685
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-18 18:42:07 -0700

    upated task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * main - Print a text and a new line
@@ -7,7 +6,18 @@
  * Return - 0
 */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+
+	return (0);
 }
