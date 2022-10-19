847bf1b9a19ce8f578f2bc16c1f4c62013d31daf
odafe alaiya <alaiyaodafe@gmail.com>
2022-10-18 18:25:07 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 
 /**
  * main - Print a text and a new line
@@ -6,9 +7,7 @@
  * Return - 0
 */
 
-int main(void)
+int _putchar(char c)
 {
-	printf("_putchar\n");
-
-	return (0);
+	return (write(1, &c, 1));
 }
